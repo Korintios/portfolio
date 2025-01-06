92ccb68cbdce4875d40d90c526d0de3babf652b4
Juan Vergara <eljuankixx1354@hotmail.com>
2025-01-06 12:18:51 -0500

    add upload files

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer () {
+    return (
+        <section className="flex flex-col items-center justify-center h-[50vh] gap-6">
+            <div className="flex flex-col items-center justify-center gap-5">
+                <h1 className="text-6xl font-bold text-center w-[900px]">¿Tienes una idea de proyecto increíble? <b className="text-gradient-blue">Discutamos</b></h1>
+            </div>
+        </section>
+    )
+}
