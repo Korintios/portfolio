fb8b58ec6213d98879a502ef67fbd99300ea18c0
Juan Vergara <eljuankixx1354@hotmail.com>
2025-04-16 12:36:34 -0500

    update Portfolio component

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,9 +1,11 @@
+import AboutMe from "./aboutMe/AboutMe";
 import Presentation from "./Presentation";
 
 export default function Portfolio() {
     return (
         <div className="bg-black h-auto">
             <Presentation/>
+            <AboutMe/>
         </div>
     )
 }
