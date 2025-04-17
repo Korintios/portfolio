af4e66f88031725ca0a18eb4573119ed120758f3
Juan Vergara <eljuankixx1354@hotmail.com>
2025-04-17 15:38:24 -0500

    remove types and add new

---
+++ src/constants/badges.ts
@@ -0,0 +1,14 @@
+import { badges } from "../types";
+
+export const BADGE_COLORS: Record<badges, string> = {
+	html: "#8c2d13",
+	css: "#0873c0",
+	javascript: "#9c8c00",
+	react: "#58c4dc",
+	nodejs: "#47602c",
+	tailwindcss: "#0096cc",
+	nextjs: "#111111",
+	typescript: "#003c66",
+	sqlite: "#1a3b5a",
+	prisma: "#0c1c25",
+};
