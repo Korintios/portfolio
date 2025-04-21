85826947a6cae214925b3b69d418d45ac0e88fbb
Juan Vergara <eljuankixx1354@hotmail.com>
2025-04-21 13:48:50 -0500

    add new constants

---
+++ src/constants/skills.ts
@@ -0,0 +1,20 @@
+import JavaScript from "../assets/icons/skills/JavaScript";
+import NextJS from "../assets/icons/skills/NextJS";
+import React from "../assets/icons/skills/React";
+
+const SKILLS = [
+    {
+        name: "JavaScript",
+        icon: JavaScript,
+    },
+    {
+        name: "React",
+        icon: React,
+    },
+    {
+        name: "Next.js",
+        icon: NextJS,
+    }
+]
+
+export default SKILLS;
--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -1,22 +1,29 @@
-export const SOCIALS = [
+import Email from "../assets/icons/socials/Email";
+import Github from "../assets/icons/socials/Github";
+import Linkedin from "../assets/icons/socials/Linkedin";
+import Phone from "../assets/icons/socials/Phone";
+
+const SOCIALS = [
     {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/juancamilovm/",
-        icon: "icons/linkedin.svg",
+        url: "https://www.linkedin.com/in/juan-camilo-vergara-marin-a50805263/",
+        icon: Linkedin,
     },
     {
         name: "GitHub",
         url: "https://github.com/Korintios",
-        icon: "icons/github.svg",
+        icon: Github,
     },
     {
-        name: "WhatsApp",
+        name: "Phone",
         url: "https://wa.me/573147685292?text=Hola%2C%20me%20interesa%20conocer%20m%C3%A1s%20sobre%20los%20servicios%20que%20ofreces%2C%20Juan%20Camilo%20V.M.%20%C2%BFPodr%C3%ADas%20darme%20m%C3%A1s%20informaci%C3%B3n%3F",
-        icon: "icons/phone.svg",
+        icon: Phone,
     },
     {
         name: "Email",
         url: "mailto:juanvergaram@outlook.com",
-        icon: "icons/email.svg",
+        icon: Email,
     }
 ]
+
+export default SOCIALS;
